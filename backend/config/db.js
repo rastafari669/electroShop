8a23bbc9c07fedeeb2986324729aa5d655831a2a
hugendubell <joeykraus789@gmail.com>
2021-01-26 18:38:36 +0200

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () =>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
+        const conn = await mongoose.connect('mongodb+srv://joey789:joey789@cluster0.xha8k.mongodb.net/elecShop?retryWrites=true&w=majority',{
             useUnifiedTopology:true,
             useNewUrlParser:true,
             useCreateIndex: true
